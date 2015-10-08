2fa2b72e9a093ef739ab0c6712390c420a0858a1
Squareys <Squareys@googlemail.com>
2015-10-08 13:33:48 +0200

    Add OutputColumnMappingKnimePostprocessor
    Signed-off-by: Squareys <Squareys@googlemail.com>

--- org.knime.knip.scijava.commands/src/org/knime/knip/scijava/commands/mapping/OutputColumnMappingKnimePostprocessor.java
+++ org.knime.knip.scijava.commands/src/org/knime/knip/scijava/commands/mapping/OutputColumnMappingKnimePostprocessor.java
@@ -1,11 +1,18 @@
-package org.knime.knip.scijava.commands;
+package org.knime.knip.scijava.commands.mapping;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import org.knime.core.data.DataCell;
+import org.knime.core.data.DataColumnSpec;
+import org.knime.core.data.DataColumnSpecCreator;
+import org.knime.core.data.DataTable;
 import org.knime.core.data.DataTableSpec;
+import org.knime.core.data.DataType;
 import org.knime.core.data.def.DefaultRow;
+import org.knime.knip.scijava.commands.InputDataRowService;
+import org.knime.knip.scijava.commands.KnimePostprocessor;
+import org.knime.knip.scijava.commands.OutputDataRowService;
 import org.knime.knip.scijava.commands.adapter.OutputAdapter;
 import org.knime.knip.scijava.commands.adapter.OutputAdapterService;
 import org.scijava.Priority;
@@ -19,19 +26,16 @@ import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 
 /**
- * Default implementation of KnimePostprocessor.
- * 
- * Requires {@link OutputAdapterService}, {@link InputDataRowService} and
- * {@link OutputDataRowService}.
+ * Preprocessor which fills unresolved {@link Module} inputs from KNIME
+ * {@link DataTable}s using {@link ColumnModuleItemMapping}s from a
+ * {@link OutputToColumnMappingService}.
  * 
  * @author Jonathan Hale (University of Konstanz)
- * 
  */
-@Plugin(type = PostprocessorPlugin.class, priority = DefaultKnimePostprocessor.PRIORITY)
-public class DefaultKnimePostprocessor extends AbstractPostprocessorPlugin
-		implements KnimePostprocessor {
-
-	public static final double PRIORITY = Priority.NORMAL_PRIORITY;
+@Plugin(type = PostprocessorPlugin.class, priority = Priority.NORMAL_PRIORITY
+		+ 1)
+public class OutputColumnMappingKnimePostprocessor
+		extends AbstractPostprocessorPlugin implements KnimePostprocessor {
 
 	@Parameter
 	private OutputAdapterService adapterService;
@@ -43,12 +47,15 @@ public class DefaultKnimePostprocessor extends AbstractPostprocessorPlugin
 	private OutputDataRowService dataRowOut;
 
 	@Parameter
+	private OutputToColumnMappingService outputMappingService;
+
+	@Parameter
 	private LogService log;
 
 	/**
 	 * Straight forward implementation of module output to DataRow: For every
-	 * output find an OutputAdapter to create a DataCell from it and create a
-	 * DefaultDataRow from created DataCells.
+	 * output which has a mapping find an OutputAdapter to create a DataCell
+	 * from it and create a DefaultDataRow from created DataCells.
 	 * 
 	 * {@inheritDoc}
 	 */
@@ -59,6 +66,11 @@ public class DefaultKnimePostprocessor extends AbstractPostprocessorPlugin
 		List<DataCell> cells = new ArrayList<DataCell>();
 
 		for (ModuleItem i : module.getInfo().outputs()) {
+			if (outputMappingService.getMappingForModuleItem(i) == null) {
+				// No mapping for this output. Will not create a cell.
+				continue;
+			}
+
 			final OutputAdapter<?, DataCell> outputAdapter = adapterService
 					.getMatchingOutputAdapter(i.getType());
 
@@ -71,7 +83,7 @@ public class DefaultKnimePostprocessor extends AbstractPostprocessorPlugin
 			}
 		}
 
-		dataRowOut.setOutputDataRow(new DefaultRow(dataRowIn.getInputDataRow()
-				.getKey(), cells));
+		dataRowOut.setOutputDataRow(
+				new DefaultRow(dataRowIn.getInputDataRow().getKey(), cells));
 	}
 }
