779a7db1be48500a0034559e951b98bfabc6d2ac
Squareys <Squareys@googlemail.com>
2015-10-12 11:27:27 +0200

    Use WeakReference in DataRowServices
    Signed-off-by: Squareys <Squareys@googlemail.com>

--- org.knime.knip.scijava.commands/src/org/knime/knip/scijava/commands/DefaultOutputDataRowService.java
+++ org.knime.knip.scijava.commands/src/org/knime/knip/scijava/commands/DefaultOutputDataRowService.java
@@ -1,41 +1,45 @@
 package org.knime.knip.scijava.commands;
 
+import java.lang.ref.WeakReference;
+
 import org.knime.core.data.DataRow;
 import org.scijava.Priority;
 import org.scijava.plugin.Plugin;
 import org.scijava.service.AbstractService;
 
 /**
- * Default implementation of OutputDataRowService.
+ * Default implementation of OutputDataRowService. Holds a {@link WeakReference}
+ * to a {@link DataRow} to ensure that it can be garbage collected once the
+ * {@link DataRow} is not referenced outside of this service.
  * 
  * @author Jonathan Hale (University of Konstanz)
  * 
  */
 @Plugin(type = OutputDataRowService.class, priority = DefaultOutputDataRowService.PRIORITY)
-public class DefaultOutputDataRowService extends AbstractService implements
-		OutputDataRowService {
+public class DefaultOutputDataRowService extends AbstractService
+		implements OutputDataRowService {
 
 	/**
 	 * Priority of this {@link Plugin}
 	 */
 	public static final double PRIORITY = Priority.NORMAL_PRIORITY;
 
-	private DataRow m_row = null;
+	private WeakReference<DataRow> m_row = new WeakReference<>(null);
 
 	/**
-	 *{@inheritDoc}
+	 * {@inheritDoc}
 	 */
 	@Override
 	public void setOutputDataRow(DataRow r) {
-		m_row = r;
+		m_row = new WeakReference<>(r);
 	}
 
 	/**
-	 *{@inheritDoc}
+	 * {@inheritDoc}
 	 */
 	@Override
 	public DataRow getOutputDataRow() {
-		return m_row;
+		return m_row.get();
 	}
 
 }
