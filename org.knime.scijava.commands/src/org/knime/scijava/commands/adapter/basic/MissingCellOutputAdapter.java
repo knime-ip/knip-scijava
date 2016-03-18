1ed5dc31bee03f586bad2524e20ccb632b5af3c2
Gabriel Einsdorf <gabriel.einsdorf@uni-konstanz.de>
2016-03-18 17:08:07 -0500

    Fix MissingValue output adapter.

--- org.knime.scijava.commands/src/org/knime/scijava/commands/adapter/basic/MissingCellOutputAdapter.java
+++ org.knime.scijava.commands/src/org/knime/scijava/commands/adapter/basic/MissingCellOutputAdapter.java
@@ -1,26 +1,26 @@
 package org.knime.scijava.commands.adapter.basic;
 
 import org.knime.core.data.MissingCell;
-import org.knime.core.data.MissingValue;
 import org.knime.scijava.commands.adapter.AbstractOutputAdapter;
 import org.knime.scijava.commands.adapter.OutputAdapter;
 import org.scijava.Priority;
 import org.scijava.plugin.Plugin;
 
 /**
- * Fallback adapter for "result" output parameters.
+ * Fall-back adapter for module outputs of type "Object", will create a missing
+ * value
  *
  * @author gabriel
  *
  */
 
 @Plugin(type = OutputAdapter.class, priority = Priority.VERY_LOW_PRIORITY)
-public class ObjectOutputAdapter
-		extends AbstractOutputAdapter<Object, MissingValue> {
+public class MissingCellOutputAdapter
+		extends AbstractOutputAdapter<Object, MissingCell> {
 
 	@Override
-	public Class<MissingValue> getOutputType() {
-		return MissingValue.class;
+	public Class<MissingCell> getOutputType() {
+		return MissingCell.class;
 	}
 
 	@Override
