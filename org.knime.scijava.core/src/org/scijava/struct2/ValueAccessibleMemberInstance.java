385ee93521f6d5126ae8b10f3209faba7b1764ae
Marcel Wiedenmann <marcel.wiedenmann@knime.com>
2018-01-25 01:34:02 +0100

    WIP: add hack to ValueAccessibleMemberInstance#set(Object)
    This is needed to allow boxed values when the member's type is
    primitive.

--- org.knime.scijava.core/src/org/scijava/struct2/ValueAccessibleMemberInstance.java
+++ org.knime.scijava.core/src/org/scijava/struct2/ValueAccessibleMemberInstance.java
@@ -1,16 +1,18 @@
 package org.scijava.struct2;
 
+import java.lang.reflect.Type;
+
+import org.apache.commons.lang3.ClassUtils;
 import org.scijava.util2.Types;
 
 public class ValueAccessibleMemberInstance<T> implements MemberInstance<T> {
 
-	private Member<T> member;
-	private ValueAccessible<T> access;
-	private Object object;
+	private final Member<T> member;
+	private final ValueAccessible<T> access;
+	private final Object object;
 
-	public <M extends Member<T> & ValueAccessible<T>> ValueAccessibleMemberInstance(
-		final M member, final Object object)
-	{
+	public <M extends Member<T> & ValueAccessible<T>> ValueAccessibleMemberInstance(final M member,
+			final Object object) {
 		this.member = member;
 		this.access = member;
 		this.object = object;
@@ -37,12 +39,16 @@ public class ValueAccessibleMemberInstance<T> implements MemberInstance<T> {
 	}
 
 	@Override
-	public void set(Object value) {
-		final Class<?> type = member().getRawType();
-		if (!Types.isAssignable(value.getClass(), member().getType())) {
+	public void set(final Object value) {
+		// HACK: convert primitive type to wrapper type to ensure assignability (which should be legal here)
+		Type type = member().getType();
+		if (type instanceof Class) {
+			type = ClassUtils.primitiveToWrapper((Class<?>) type);
+		}
+		if (!Types.isAssignable(value.getClass(), type)) {
 			throw new IllegalArgumentException("value of type " + //
-				Types.name(value.getClass()) + " is not assignable to " + //
-				Types.name(type));
+					Types.name(value.getClass()) + " is not assignable to " + //
+					Types.name(member().getRawType()));
 		}
 		@SuppressWarnings("unchecked")
 		final T tValue = (T) value;
